25fc1c007cf5ab21dd397e23807342a8555719d8
gabriel-farias <gabrielfariasaular@gmail.com>
2023-10-22 00:57:22 -0300

    Declaracion de Variable

---
+++ resources/js/codigo.js
@@ -0,0 +1,8 @@
+// alert("Pop up");
+
+variableNombre=prompt("hola dime tu nombre", "nombre");
+
+console.log("El nombre ingresado es: " + variableNombre);
+// console.warn(variableNombre);
+// console.info(variableNombre);
+// console.error(variableNombre);
