83b3fecd5f9b2ff8bb3156a2e0e7e109c9982f47
gabriel-farias <gabrielfariasaular@gmail.com>
2023-10-22 01:06:00 -0300

    Modificacion del JS

--- resources/js/codigo.js
+++
@@ -1,8 +0,0 @@
-// alert("Pop up");
-
-variableNombre=prompt("hola dime tu nombre", "nombre");
-
-console.log("El nombre ingresado es: " + variableNombre);
-// console.warn(variableNombre);
-// console.info(variableNombre);
-// console.error(variableNombre);
